02929ff60f3a2d9723b8c0d951cf00af5af2d951
taina <tainajesus533@gmail.com>
2024-10-21 16:38:05 -0300

    busca de cep

--- js/script.js
+++ js/script.js
@@ -3,32 +3,32 @@ document.addEventListener('DOMContentLoaded',()=>{
     const cepinput = document.getElementById('cep-input');
     const addressInfo = document.getElementById('address-info');
 
-    searchbutton.addEventListener('click',() =>{
+    searchbutton.addEventListener('click', () => {
         const cep = cepInput.value.replace (/\D/g, '');
 
-        if (cep. length !==8) {
+        if (cep. length !== 8) {
             alert('por favor, insira um CEP válido com 8 dígitos.');
             return;
         }
-        const apiUrl ='https:viacep.com.br/ws/${cep}/json/';
+        const apiUrl =`https:viacep.com.br/ws/${cep}/json/`;
 
         fetch(apiUrl)
         .then(response => response.json())
-        .then(data =>{
-             if(data.erro){
-                alert('CEP não encontrado. Verifique o CEP digitado');
-             } else{
-                const addressHTML =`
-                <h2> Endereço Encontrado:</h2>
-                <p>CEP ${data.cep}</p>
+        .then(data => {
+             if (data.erro) {
+                alert('CEP não encontrado. Verifique o CEP digitado.');
+             } else {
+                const addressHTML = `
+                <h2>Endereço Encontrado:</h2>
+                <p>CEP: ${data.cep}</p>
                 <p>logradouro: ${data.logradouro}</p>
-                <p>bairro: ${data.barro}</p>
-                <p>cidade/UF ${data.localidade}/${data.uf}</p>
+                <p>bairro: ${data.bairro}</p>
+                <p>cidade/UF: ${data.localidade}/${data.uf}</p>
                 `;
                 addressInfo.innerHTML = addressHTML;
-                }
+              } 
         })
-        .catch(erro =>{
+        .catch(erro => {
             console.error('erro ao buscar o endereço:', error);
             alert('não foi possível obter o endereço. Verifique sua conexão com a internet.');
         });
